9bec06ee89cb8921405a8cef09a9866a61663bd4
Franx0 <francisco.moya.martinez@gmail.com>
2021-04-14 18:20:00 +0000

    add card shadows && increment fonst size

--- components/shared/card.tsx
+++ components/shared/card.tsx
@@ -13,7 +13,7 @@ const Card: FunctionComponent<any> = (props: CardProps) => {
       {
         cardOpened ? (
           <motion.div className={`md:p-12 content-center expanded-card ${className}`} layoutId="expandable-card" style={expandedStyle}>
-            <h2 className="absolute top-0 right-0 text-right p-3 text-current mr-1 cursor-pointer" onClick={() => handleCardOpened("#")}>X</h2>
+            <button className="absolute top-0 right-0 text-right p-3 text-current mr-1 cursor-pointer" title="Close" type="button" onClick={() => handleCardOpened("#")}>X</button>
             <div className="lazy-text">
               <motion.h2 className="expanded-card-h text-current mb-2" layoutId="expandable-card-h">{title}</motion.h2>
               <hr className="min-w-full mb-8 border-current lazy-grow origin-left" />
@@ -21,7 +21,7 @@ const Card: FunctionComponent<any> = (props: CardProps) => {
             </div>
           </motion.div>
         ) : (
-          <motion.a title={image.alt} onClick={() => handleCardOpened()} className={`cursor-pointer normal-card ${className}`} layoutId="expandable-card" style={style}>
+          <motion.a title={image.alt} onClick={() => handleCardOpened()} className={`shadow-lg cursor-pointer normal-card ${className}`} layoutId="expandable-card" style={style}>
             <img width={image.width} height={image.height} src={image.url} alt={image.alt} loading="lazy" className="mx-auto align-center rounded-full" />
           </motion.a>
         )
