48649f72a081ecc73c7b71dc2024af07b9b708cd
Franx0 <francisco.moya.martinez@gmail.com>
2021-04-21 12:20:11 +0000

    update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -36,14 +36,16 @@ export const Layout = ({
         <meta name="description" content={locale.dictionary.meta[router.pathname.replace("/", "")].description} />
         <meta name="build version" content={version} />
         <meta name="robots" content="index,follow" />
-        <meta name="og:type" content="website" />
-        <meta name="og:url" content={process.env.NEXT_PUBLIC_AUTH_URL} />
-        <meta name="og:image" content={locale.dictionary.meta[router.pathname.replace("/", "")].image_url} key="image" />
-        <meta name="og:title" content={locale.dictionary.meta[router.pathname.replace("/", "")].title} key="title" />
-        <meta name="og:description" content={locale.dictionary.meta[router.pathname.replace("/", "")].description} key="description" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_AUTH_URL} />
+        <meta property="og:image" content={locale.dictionary.meta[router.pathname.replace("/", "")].image_url} key="image" />
+        <meta property="og:title" content={locale.dictionary.meta[router.pathname.replace("/", "")].title} key="title" />
+        <meta property="og:description" content={locale.dictionary.meta[router.pathname.replace("/", "")].description} key="description" />
         <meta name="twitter:card" content="twitter-summary" />
         <meta name="twitter:creator" content="Francisco Moya" key="twitter-creator" />
-        <meta name="twitter:site" content={process.env.NEXT_PUBLIC_AUTH_URL} key="twitter-site" />
+        <meta property="twitter:domain" content={process.env.NEXT_PUBLIC_DOMAIN} key="twitter-site" />
+        <meta property="twitter:url" content={process.env.NEXT_PUBLIC_AUTH_URL} key="twitter-site" />
+        <meta name="twitter:image" content={locale.dictionary.meta[router.pathname.replace("/", "")].image_url} key="twitter-image" />
         <meta name="twitter:title" content={locale.dictionary.meta[router.pathname.replace("/", "")].title} key="twitter-title" />
         <meta name="twitter:description" content={locale.dictionary.meta[router.pathname.replace("/", "")].description} key="twitter-description" />
       </Head>
