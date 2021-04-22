e7ed9f0a9792ad822aa1c5b720c6d7e537f242a7
Franx0 <francisco.moya.martinez@gmail.com>
2021-04-22 17:21:16 +0000

    update meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -40,17 +40,11 @@ export const Layout = ({
         <meta name="description" content={meta.description} />
         <meta name="build version" content={version} />
         <meta name="robots" content="index,follow" />
+
         {/* Google */}
         <meta itemProp="name" content={meta.title} key="title" />
         <meta itemProp="description" content={meta.description} key="desc" />
         <meta itemProp="image" content={meta.imageUrl} key="image" />
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" key="twcard" />
-        <meta name="twitter:creator" content="@Franxo06" key="twhandle" />
-        <meta name="twitter:image" content={meta.imageUrl} key="twimage" />
-        <meta name="twitter:title" content={meta.title} key="twtitle" />
-        <meta name="twitter:description" content={meta.description} key="twdesc" />
 
         {/* Open Graph */}
         <meta property="og:type" content="website" key="ogtype" />
@@ -58,6 +52,13 @@ export const Layout = ({
         <meta property="og:image" content={meta.imageUrl} key="ogimage" />
         <meta property="og:title" content={meta.title} key="ogtitle" />
         <meta property="og:description" content={meta.description} key="ogdesc" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" key="twcard" />
+        <meta name="twitter:creator" content="@Franxo06" key="twhandle" />
+        <meta name="twitter:image" content={meta.imageUrl} key="twimage" />
+        <meta name="twitter:title" content={meta.title} key="twtitle" />
+        <meta name="twitter:description" content={meta.description} key="twdesc" />
 
         <link rel="icon" href={`/favicon/favicon-${theme}.ico`} />
         <link rel="icon" type="image/png" sizes="32x32" href={`/favicon/favicon-32x32-${theme}.png`} />
