bbd780cc420321633f62ed0613f015a65acc1e90
Franx0 <francisco.moya.martinez@gmail.com>
2021-04-23 11:14:26 +0000

    fix meta working fine build 2

--- pages/500.tsx
+++ pages/500.tsx
@@ -8,7 +8,7 @@ const Layout = dynamic(() => import('@/components/Layout'), {
 
 const Custom500Page: NextPage<NextPageContext> = () => {
   return (
-    <Layout title="500" className="w-full mx-6 my-6 md:mx-0">
+    <Layout className="w-full mx-6 my-6 md:mx-0">
       {(locale: any) =>
         <div className="flex md:flex-row flex-col h-full align-middle justify-center items-center m-auto">
           <h1 className="md:text-5xl text-xl">500</h1>
