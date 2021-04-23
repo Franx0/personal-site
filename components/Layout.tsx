8dbb9d41448da3c4ce83b6f4459d4b3b73c8c639
Franx0 <francisco.moya.martinez@gmail.com>
2021-04-23 10:33:59 +0000

    remove fragment to div

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -32,7 +32,7 @@ export const Layout = ({
   };
 
   return (
-    <>
+    <div>
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -81,7 +81,7 @@ export const Layout = ({
       <Cookies />
 
       <Footer {...{locale}} />
-    </>
+    </div>
   )
 }
 
