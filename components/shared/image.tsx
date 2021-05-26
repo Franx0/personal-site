79d0057444eb5d974b80a970bdab85058f9afadf
Franx0 <francisco.moya.martinez@gmail.com>
2021-05-26 09:35:55 +0000

    update image component missing src

--- components/shared/image.tsx
+++ components/shared/image.tsx
@@ -3,11 +3,14 @@ import { FunctionComponent, useState, useEffect } from 'react';
 // Types
 import { ImageProps } from '@/interfaces/index';
 
+const defaultImageUrl = '/media.jpg';
 
 const Image: FunctionComponent<any> = (props: ImageProps) => {
   const { className, style, src, defaultSrc, alt, width="100", height="100"} = props;
-  const defaultSrcObject = { src: (src || '/media.jpg'), blurred: true };
-  const [currentSrc, setCurrentSrc] = useState(defaultSrcObject);
+  const defaultSrcObject = { src: defaultImageUrl, blurred: true };
+  const [currentSrc, setCurrentSrc] = useState(
+    {...defaultSrcObject, src: (src || defaultSrc || defaultImageUrl)}
+  );
 
   useEffect(() => {
     if(src) setCurrentSrc({ src: src, blurred: false });
