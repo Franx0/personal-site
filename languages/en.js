dd00dcdfe2eb3729137939b194b8265305bfdcad
Fran Moya <fran@devengo.com>
2021-11-05 10:46:43 +0100

    update images and cv links ids

--- languages/en.js
+++ languages/en.js
@@ -46,7 +46,7 @@ export default {
     title: `Loading`,
     text: `This may take a few seconds, please don't close this page.`
   },
-  profile_image_url: "https://drive.google.com/file/d/1_PEQK-Gk3-tKVN_TYZ9p_l45TA217Fnf/view?usp=sharing",
+  profile_image_url: "https://drive.google.com/uc?export=view&id=1VvS8hsWKHVrdYzFoCl--5bx5gLkTq4aS",
   header: {
     links: {
       home: 'Home',
@@ -65,7 +65,7 @@ export default {
   },
   cv: {
     download: `Download CV`,
-    download_url: `https://drive.google.com/file/d/1TqmD0eo7ZVdR1xk2hbD-V2LE59oitCke/view?usp=sharing`,
+    download_url: `https://drive.google.com/uc?export=view&id=1TqmD0eo7ZVdR1xk2hbD-V2LE59oitCke`,
     description: `<h1 class="text-accent">Hi, I'm Fran.</h1><br><p>I am a web developer who is always looking for new challenges and projects to be part of.</p><p>During the last 6 years, I have been working as a developer, learning and improving my knowledge and skills. I love to keep learning and sharing it with all people around me</p><p>Take a look to some of the languages and technologies I usually work to and <a title="Mail" href=mailto:${process.env.NEXT_PUBLIC_GMAIL_ACCOUNT}?subject=${encodeURIComponent('Hi Fran!')}><strong>contact me</strong></a>.</p>`,
     favorites: `My favorites`,
     myJobs: `My jobs`,
@@ -76,35 +76,35 @@ export default {
         description: `I started as a Junior developer on People Who in 2015. We developed a health web application with Ruby on Rails, Jquery and MySQL and later evolved it with React and Redis to improve our UI and processes performance. I learnt a lot thanks to the team we were.`,
         bgColor: theme.extend.colors.peoplewho,
         textColor: theme.extend.colors.light.gray.secondary,
-        imageUrl: `https://drive.google.com/file/d/1c3LycjoOWWZHaeVXHXDE-6J2OZUcy3ze/view?usp=sharing`
+        imageUrl: `https://drive.google.com/uc?export=view&id=1c3LycjoOWWZHaeVXHXDE-6J2OZUcy3ze`
       },
       "Never Empty": {
         subtitle: `Nov. 2016 - Feb. 2017`,
         description: `It was a fleeting experience because of the company status but also a enrichment time to me. I worked again with Ruby on Rails and PostgreSQL, with the servers running on Heroku and migrating the current frontside inside the monolithic application to a new and fancy React environment.`,
         bgColor: theme.extend.colors.neverempty,
         textColor: theme.extend.colors.light.white.secondary,
-        imageUrl: `https://drive.google.com/file/d/1gk_cx9Gf2jN-QPA5ekPcYcl2PUBRtmWV/view?usp=sharing`
+        imageUrl: `https://drive.google.com/uc?export=view&id=1gk_cx9Gf2jN-QPA5ekPcYcl2PUBRtmWV`
       },
       "Informa": {
         subtitle: `Feb. 2017 - Jun. 2017`,
         description: `After a couple of years of working experience  I decided to give a chance to the consultancy sector. I became part of Informa D&B as an external consultory. I started working in a small team focused on migrate an internal tool built on Ruby on Rails to a new React/Redux/Saga application, I also helped to migrate the current Ruby on Rails API to Clojure and optimize the frontend performance with Elastic Search.`,
         bgColor: theme.extend.colors.informa,
         textColor: theme.extend.colors.light.gray.secondary,
-        imageUrl: `https://drive.google.com/file/d/1dyOOHyVuHhmh7tGDRt-S8dG84LmtF0iC/view?usp=sharing`
+        imageUrl: `https://drive.google.com/uc?export=view&id=1dyOOHyVuHhmh7tGDRt-S8dG84LmtF0iC`
       },
       "Glownet": {
         subtitle: `Jun. 2017 - Nov. 2019`,
         description: `In Glownet we developed a RFID system to run multiple events with no need of internet connection. Sònar, Coachella, MadCool and many others trusted on Glownet to run their festivals. The Android team developed the Mobile App to control onsite everything that happened inside the events and the 'Cloud' team developed the neccesary APIs, administration tool and the events analytics system. Ruby on Rails was the chosen technology to do that, with a powerfull database system based on PostgreSQL and the servers running in Amazon AWS.`,
         bgColor: theme.extend.colors.glownet,
         textColor: theme.extend.colors.light.white.secondary,
-        imageUrl: `https://drive.google.com/file/d/1h8YbddSVzHYbKaheXA1mW92gO6lx1x82/view?usp=sharing`
+        imageUrl: `https://drive.google.com/uc?export=view&id=1h8YbddSVzHYbKaheXA1mW92gO6lx1x82`
       },
       "String Projects": {
         subtitle: `Feb. 2020 - Nov. 2020`,
         description: `After my last adventure I started working on String Projects, a tiny tech consultory but with heavy clients. We worked with many different technologies and languages, depending of what client needed. React Native was the ideal to develop mobile apps and Ruby on Rails the ideal to develop strong and powefull backends. But as I said, it changed depending on the client needs.`,
         bgColor: theme.extend.colors.string_,
         textColor: theme.extend.colors.light.white.secondary,
-        imageUrl: `https://drive.google.com/file/d/1VX8vLt6jeG6i8zaNGpOobewLLF7-PV6X/view?usp=sharing`
+        imageUrl: `https://drive.google.com/uc?export=view&id=1VX8vLt6jeG6i8zaNGpOobewLLF7-PV6X`
       }
     }
   },
