1e560320a6e2ade8587ea7bc9956ad47d9493a98
Franx0 <francisco.moya.martinez@gmail.com>
2023-04-12 20:58:56 +0200

    Include target blank to all footer icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,10 +9,10 @@ export const Footer = (props: any) => {
           <div className="md:flex px-4">
             <div className="flex justify-center md:justify-start m-3 min-w-min">
               <p className="hidden md:flex md:align-middle md:m-auto md:ml-0 md:mr-0">{props.dictionary.footer.follow}</p>
-              <a href={`https://${process.env.NEXT_PUBLIC_GITHUB_ACCOUNT}`} style={{paddingLeft: "0.05rem"}} className="w-8 h-8 text-default hover:text-white border border-2 border-gray-400 rounded-full text-center ml-2 hover:bg-black hover:border-black">
+              <a href={`https://${process.env.NEXT_PUBLIC_GITHUB_ACCOUNT}`} target="_blank" style={{paddingLeft: "0.05rem"}} className="w-8 h-8 text-default hover:text-white border border-2 border-gray-400 rounded-full text-center ml-2 hover:bg-black hover:border-black">
                 <GitHub width={"w-7"} height={"h-7"} />
               </a>
-              <a href={`https://${process.env.NEXT_PUBLIC_LINKEDIN_ACCOUNT}`} className="w-8 h-8 text-default hover:text-white border border-2 border-gray-400 rounded-full text-center px-2 py-1.5 ml-2 hover:bg-blue-600 hover:border-blue-600">
+              <a href={`https://${process.env.NEXT_PUBLIC_LINKEDIN_ACCOUNT}`} target="_blank" className="w-8 h-8 text-default hover:text-white border border-2 border-gray-400 rounded-full text-center px-2 py-1.5 ml-2 hover:bg-blue-600 hover:border-blue-600">
                 <Linkedin width={"w-4"} height={"h-4"} />
               </a>
               <a href={`https://twitter.com/intent/follow?original_referer=https://${process.env.NEXT_PUBLIC_DOMAIN}&ref_src=twsrc%5Etfw&region=follow_link&screen_name=${process.env.NEXT_PUBLIC_TWITTER_ACCOUNT}&tw_p=followbutton`}
